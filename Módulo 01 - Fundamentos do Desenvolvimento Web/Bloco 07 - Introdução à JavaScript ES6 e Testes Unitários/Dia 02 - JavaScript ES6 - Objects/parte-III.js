edf616d9e7b93236c7d5aed10253d1db2dfcc88b
Otto Micheletti <michelettiotto@gmail.com>
2022-01-14 15:29:35 -0300

    7.3 - Exercícios

--- Módulo 01 - Fundamentos do Desenvolvimento Web/Bloco 07 - Introdução à JavaScript ES6 e Testes Unitários/Dia 02 - JavaScript ES6 - Objects/parte-III.js
+++ Módulo 01 - Fundamentos do Desenvolvimento Web/Bloco 07 - Introdução à JavaScript ES6 e Testes Unitários/Dia 02 - JavaScript ES6 - Objects/parte-III.js
@@ -27,24 +27,26 @@ console.table(lesson2);
 
 // 2.
 const listaKeys = (obj) => {
-  console.log(`As chaves do objeto são: ${(Object.keys(obj)).join(', ')}`);
+  console.log(`As chaves do objeto são: ${Object.keys(obj).join(', ')}`);
 };
 listaKeys(lesson2);
 
 // 3.
 const tamanhoObj = (obj) => {
   console.log(`O tamanho do objeto é: ${Object.keys(obj).length}`);
-}
+};
 tamanhoObj(lesson2);
 
 // 4.
 const valoresObj = (obj) => {
-  console.log(`Os valores do objeto em questão são: ${Object.values(obj).join(', ')}`);
-}
+  console.log(
+    `Os valores do objeto em questão são: ${Object.values(obj).join(', ')}`
+  );
+};
 valoresObj(lesson2);
 
 // 5.
-const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
+const allLessons = Object.assign({}, { lesson1, lesson2, lesson3 });
 console.log(allLessons);
 
 // 6.
@@ -52,34 +54,42 @@ const totalAlunos = (obj) => {
   let resultado = 0;
   const array = Object.keys(obj);
   for (const index in array) {
-   resultado += obj[array[index]].numeroEstudantes;
+    resultado += obj[array[index]].numeroEstudantes;
   }
-  return console.log(`No total são ${resultado} estudantes em todas as aulas`);;
-}
+  return console.log(`No total são ${resultado} estudantes em todas as aulas`);
+};
 totalAlunos(allLessons);
 
 // 7.
 const descobreValor = (obj, n) => {
   const array = Object.values(obj);
-  console.log(`O valor encontrado foi: ${array[n]}`)
-}
+  console.log(`O valor encontrado foi: ${array[n]}`);
+};
 descobreValor(lesson2, 2);
 
 // 8.
 const verificaValor = (obj, key, value) => {
-  obj[key] === value ? console.log(`O valor '${value}' pesquisado na chave '${key}' do objeto em questão é: verdadeiro`) : console.log(`O valor '${value}' pesquisado na chave '${key}' do objeto em questão é: falso`);
-}
+  obj[key] === value
+    ? console.log(
+        `O valor '${value}' pesquisado na chave '${key}' do objeto em questão é: verdadeiro`
+      )
+    : console.log(
+        `O valor '${value}' pesquisado na chave '${key}' do objeto em questão é: falso`
+      );
+};
 verificaValor(lesson3, 'turno', 'noite');
 
 // Bônus 1.
 const verificaMateria = (obj, sbj) => {
   let resultado = 0;
   const array = Object.keys(obj);
-  for(const index in array){
-    if(obj[array[index]].materia === sbj){
+  for (const index in array) {
+    if (obj[array[index]].materia === sbj) {
       resultado += parseInt(obj[array[index]].numeroEstudantes);
     }
   }
-  return console.log(`O total de alunos que assistiram às aulas de ${sbj} é: ${resultado}`);
-}
+  return console.log(
+    `O total de alunos que assistiram às aulas de ${sbj} é: ${resultado}`
+  );
+};
 verificaMateria(allLessons, 'História');
--- README.md
+++ README.md
@@ -60,8 +60,8 @@ Repositório para guardar e monitorar todos os exercícios e projetos desenvolvi
 ##### Bloco 7: Introdução à JavaScript ES6 e Testes Unitários
 
 - [x] 7.1 - _[JavaScript ES6 - let, const, arrow functions e template literals](https://github.com/ottomicheletti/exercicios-trybe/tree/exercicios/7.1)_
-- [x] 7.2 - -[JavaScript ES6 - Objects](https://github.com/ottomicheletti/exercicios-trybe/tree/exercicios/7.2)_
-- [ ] 7.3 - Primeiros passos em Jest
+- [x] 7.2 - _[JavaScript ES6 - Objects](https://github.com/ottomicheletti/exercicios-trybe/tree/exercicios/7.2)_
+- [x] 7.3 - _[Primeiros passos em Jest](https://github.com/ottomicheletti/exercicios-trybe/tree/exercicios/7.3)_
 - [ ] 7.4 - Projeto - JavaScript Testes Unitários
 
 ##### Bloco 8: Higher Order Functions do JavaScript ES6
